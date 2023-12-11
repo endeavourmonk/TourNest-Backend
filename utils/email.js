93e3a28e3475ba2b8f01bfc6adfa330f3471365f
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2023-12-11 11:55:55 +0530

    refactored email.js to send emails to the clients for forgot password

--- utils/email.js
+++ utils/email.js
@@ -5,22 +5,19 @@ const sendEmail = async (options) => {
     service: 'Outlook',
     // host: 'smtp.forwardemail.net',
     // port: 465,
-    // secure: true,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
   });
 
-  const info = await transporter.sendMail({
+  await transporter.sendMail({
     from: 'Davjire <davjire@outlook.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
     // html: ,
   });
-
-  console.log('Message sent: %s', info.messageId);
 };
 
 module.exports = sendEmail;
