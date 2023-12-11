4aeda9c7d8ba213730a65c3ac0a18d49895af8ca
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2023-12-11 11:55:01 +0530

    refactored app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
 app.all('*', (req, res, next) => {
   next(new AppError(404, `${req.originalUrl} not found`));
 });
