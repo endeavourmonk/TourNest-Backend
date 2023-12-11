236e8f52b670c25e32285aaf3f3f653bdc87bb89
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2023-12-11 14:33:54 +0530

    added update-me route handler to update the login user data by filtering out the sensitive properties

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,14 @@ const User = require('../models/users');
 // const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 
+const filterObj = (obj, allowedFields) => {
+  const filteredObj = {};
+  Object.keys(obj).forEach((key) => {
+    if (allowedFields.includes(key)) filteredObj[key] = obj[key];
+  });
+  return filteredObj;
+};
+
 exports.getAllUsers = async (req, res, next) => {
   try {
     const users = await User.find({});
@@ -33,12 +41,38 @@ exports.getUser = async (req, res, next) => {
     next(error);
   }
 };
+
 exports.updateUser = async (req, res, next) => {};
 exports.deleteUser = async (req, res, next) => {};
 
-exports.forgotPassword = async (req, res, next) => {
-  const { email } = req.body;
-  if (!email) return next(new AppError(400, 'Please Enter Email'));
-  const user = await User.find({ email });
-  console.log(user);
+/*  later email should be updated by asking for the current password
+    and getting verified from the new email
+*/
+exports.updateMe = async (req, res, next) => {
+  try {
+    // Don't let user to update password through this route
+    if (req.body.password || req.body.passwordConfirm)
+      return next(
+        new AppError(400, 'Password cannot be updated through this route'),
+      );
+
+    // Update the user data
+    const fieldsToKeep = ['name', 'email', 'photo'];
+    const filteredBody = filterObj(req.body, fieldsToKeep);
+
+    const { id } = req.user;
+    const updatedUser = await User.findByIdAndUpdate(id, filteredBody, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user: updatedUser,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
