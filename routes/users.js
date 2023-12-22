ec6953223996ab7a0537990ad7947ffefc37aad9
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2023-12-22 14:34:23 +0530

    added mulitple routes for user like /update-password, /update-me and /delete-user

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,33 @@
 const express = require('express');
-const { signUp, login, protect } = require('../controllers/auth');
+
+const {
+  signUp,
+  login,
+  protect,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+} = require('../controllers/auth');
 const {
   getAllUsers,
   getUser,
-  createUser,
   deleteUser,
   updateUser,
-  forgotPassword,
+  updateMe,
+  deleteMe,
 } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/login', login);
+router.post('/forgot-password', forgotPassword);
+router.patch('/reset-password/:resetToken', resetPassword);
+router.patch('/update-password', protect, updatePassword);
+router.patch('/update-me', protect, updateMe);
+router.delete('/delete-user', protect, deleteMe);
 
 router.route('/').get(protect, getAllUsers);
-router.route('/forgotpassword').post(forgotPassword);
-// router.route('/resetpassword').post(resetPassword);
-// router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
