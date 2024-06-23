a697cd5534ac6fd711fd6f4d83b57e9fc3112264
Ujjawal <ujjawal.kumar.0831@gmail.com>
2024-06-23 19:17:44 +0530

    refactored

--- utils/email.js
+++ utils/email.js
@@ -1,4 +1,5 @@
 // To send email using own email using nodemailer.
+const Resend = require('resend');
 const nodemailer = require('nodemailer');
 const path = require('path');
 const renderEmailTemplate = require('./renderEmailTemplate');
@@ -22,9 +23,6 @@ module.exports = class Email {
   createTransport_() {
     return nodemailer.createTransport({
       service: 'Outlook',
-      tls: {
-        ciphers: 'SSLv3',
-      },
       auth: {
         user: email,
         pass: password,
@@ -42,8 +40,25 @@ module.exports = class Email {
     };
 
     // create a transport and send mail.
-    await this.createTransport_().sendMail(mailOptions);
-    console.log('sent mail');
+    // await this.createTransport_().sendMail(mailOptions);
+    await new Promise((resolve, reject) => {
+      nodemailer
+        .createTransport({
+          service: 'Outlook',
+          auth: {
+            user: email,
+            pass: password,
+          },
+        })
+        .sendMail(mailOptions, (err, info) => {
+          if (err) {
+            console.error(err);
+            reject(err);
+          } else {
+            resolve(info);
+          }
+        });
+    });
   }
 
   async sendWelcome() {
@@ -72,37 +87,56 @@ module.exports = class Email {
   }
 };
 
-// const Resend = require('resend');
-
 // const resend = new Resend.Resend(process.env.RESEND_API_KEY);
 
 // module.exports = class Email {
 //   constructor(user, url = '') {
 //     this.name = user.name.split(' ')[0];
 //     this.to = user.email;
-//     this.from = `Acme <onboarding@resend.dev>`;
+//     this.from = `${Sender} <${email}>`;
 //     this.url = url;
 //   }
 
 //   async sendMail(template, subject) {
 //     // Define mail options
 //     const mailOptions = {
-//       from: this.from,
-//       to: this.to,
+//       // from: this.from,
+//       from: 'Acme <onboarding@resend.dev>',
+//       to: 'decentlearner@gmail.com',
 //       subject,
 //       html: template,
 //     };
 
 //     const { data, error } = await resend.emails.send(mailOptions);
+//     console.log('data:');
+//     console.dir(data, { depth: null, colors: true });
+//     console.log('err:');
+//     console.dir(error, { depth: null, colors: true });
 //     if (error) throw new Error(`Error sending email`);
-
-//     // console.log('data:');
-//     // console.dir(data, { depth: null, colors: true });
-//     // console.log('err:');
-//     // console.dir(error, { depth: null, colors: true });
 //   }
 
 //   async sendWelcome() {
-//     await this.sendMail(`Welcome ${this.name}`, `Welcome to the TourNest!`);
+//     const replacements = {
+//       headerTitle: 'Welcome to TourNest!',
+//       userName: this.name,
+//       mainMessage: 'Thank you for joining us.',
+//       ctaLink: 'https://example.com',
+//       ctaText: 'Get Started',
+//     };
+//     const emailTemplate = await renderEmailTemplate(templatePath, replacements);
+//     await this.sendMail(emailTemplate, `Welcome to the TourNest!`);
+//   }
+
+//   async sendPasswordReset() {
+//     const replacements = {
+//       headerTitle: 'Password Reset Request',
+//       userName: this.name,
+//       mainMessage:
+//         'We received a request to reset your password. Click the link below to reset it. Link valid for 10 mins.',
+//       ctaLink: this.url,
+//       ctaText: 'Reset Password',
+//     };
+//     const emailTemplate = await renderEmailTemplate(templatePath, replacements);
+//     await this.sendMail(emailTemplate, `TourNest, Password Reset`);
 //   }
 // };
