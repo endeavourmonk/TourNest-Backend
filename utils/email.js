ca2d11ab6c0e265ae8700a7c035b3f132816d45c
Ujjawal <ujjawal.kumar.0831@gmail.com>
2024-06-26 13:46:20 +0530

    enabled email feature

--- utils/email.js
+++ utils/email.js
@@ -12,86 +12,6 @@ const Sender = namePart.charAt(0).toUpperCase() + namePart.slice(1);
 
 const templatePath = path.join(__dirname, '../views/emailTemplate.html');
 
-module.exports = class Email {
-  constructor(user, url = '') {
-    this.name = user.name.split(' ')[0];
-    this.to = user.email;
-    this.from = `${Sender} <${email}>`;
-    this.url = url;
-  }
-
-  createTransport_() {
-    return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      secure: false,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: email,
-        pass: password,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
-  }
-
-  async sendMail(template, subject) {
-    const mailOptions = {
-      from: this.from,
-      to: this.to,
-      subject,
-      html: template,
-    };
-
-    const transporter = this.createTransport_();
-
-    const server = await new Promise((resolve, reject) => {
-      // verify connection configuration
-      transporter.verify((error, success) => {
-        if (success) resolve(success);
-        reject(error);
-      });
-    });
-
-    if (!server) throw new Error(`Error, Failed.`);
-
-    const success = await new Promise((resolve, reject) => {
-      transporter.sendMail(mailOptions).then((info, err) => {
-        if (info.response.includes('250')) resolve(true);
-        console.log('send failed--', err);
-        reject(err);
-      });
-    });
-
-    if (!success) throw new Error(`Error sending email`);
-  }
-
-  async sendWelcome() {
-    const replacements = {
-      headerTitle: 'Welcome to TourNest!',
-      userName: this.name,
-      mainMessage: 'Thank you for joining us.',
-      ctaLink: 'https://example.com',
-      ctaText: 'Get Started',
-    };
-    const emailTemplate = await renderEmailTemplate(templatePath, replacements);
-    await this.sendMail(emailTemplate, `Welcome to the TourNest!`);
-  }
-
-  async sendPasswordReset() {
-    const replacements = {
-      headerTitle: 'Password Reset Request',
-      userName: this.name,
-      mainMessage:
-        'We received a request to reset your password. Click the link below to reset it. Link valid for 10 mins.',
-      ctaLink: this.url,
-      ctaText: 'Reset Password',
-    };
-    const emailTemplate = await renderEmailTemplate(templatePath, replacements);
-    await this.sendMail(emailTemplate, `TourNest, Password Reset`);
-  }
-};
-
 // module.exports = class Email {
 //   constructor(user, url = '') {
 //     this.name = user.name.split(' ')[0];
@@ -102,16 +22,20 @@ module.exports = class Email {
 
 //   createTransport_() {
 //     return nodemailer.createTransport({
-//       service: 'Outlook',
+//       host: process.env.EMAIL_HOST,
+//       secure: false,
+//       port: process.env.EMAIL_PORT,
 //       auth: {
 //         user: email,
 //         pass: password,
+//       },
+//       tls: {
+//         rejectUnauthorized: false,
 //       },
 //     });
 //   }
 
 //   async sendMail(template, subject) {
-//     // Define mail options
 //     const mailOptions = {
 //       from: this.from,
 //       to: this.to,
@@ -119,26 +43,27 @@ module.exports = class Email {
 //       html: template,
 //     };
 
-//     // create a transport and send mail.
-//     // await this.createTransport_().sendMail(mailOptions);
-//     await new Promise((resolve, reject) => {
-//       nodemailer
-//         .createTransport({
-//           service: 'Outlook',
-//           auth: {
-//             user: email,
-//             pass: password,
-//           },
-//         })
-//         .sendMail(mailOptions, (err, info) => {
-//           if (err) {
-//             console.error(err);
-//             reject(err);
-//           } else {
-//             resolve(info);
-//           }
-//         });
+//     const transporter = this.createTransport_();
+
+//     const server = await new Promise((resolve, reject) => {
+//       // verify connection configuration
+//       transporter.verify((error, success) => {
+//         if (success) resolve(success);
+//         reject(error);
+//       });
 //     });
+
+//     if (!server) throw new Error(`Error, Failed.`);
+
+//     const success = await new Promise((resolve, reject) => {
+//       transporter.sendMail(mailOptions).then((info, err) => {
+//         if (info.response.includes('250')) resolve(true);
+//         console.log('send failed--', err);
+//         reject(err);
+//       });
+//     });
+
+//     if (!success) throw new Error(`Error sending email`);
 //   }
 
 //   async sendWelcome() {
@@ -166,3 +91,66 @@ module.exports = class Email {
 //     await this.sendMail(emailTemplate, `TourNest, Password Reset`);
 //   }
 // };
+
+module.exports = class Email {
+  constructor(user, url = '') {
+    this.name = user.name.split(' ')[0];
+    this.to = user.email;
+    this.from = `${Sender} <${email}>`;
+    this.url = url;
+  }
+
+  createTransport_() {
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      secure: false,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: email,
+        pass: password,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+  }
+
+  async sendMail(template, subject) {
+    // Define mail options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html: template,
+    };
+
+    const transporter = this.createTransport_();
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Message sent: %s', info.messageId);
+  }
+
+  async sendWelcome() {
+    const replacements = {
+      headerTitle: 'Welcome to TourNest!',
+      userName: this.name,
+      mainMessage: 'Thank you for joining us.',
+      ctaLink: 'https://example.com',
+      ctaText: 'Get Started',
+    };
+    const emailTemplate = await renderEmailTemplate(templatePath, replacements);
+    await this.sendMail(emailTemplate, `Welcome to the TourNest!`);
+  }
+
+  async sendPasswordReset() {
+    const replacements = {
+      headerTitle: 'Password Reset Request',
+      userName: this.name,
+      mainMessage:
+        'We received a request to reset your password. Click the link below to reset it. Link valid for 10 mins.',
+      ctaLink: this.url,
+      ctaText: 'Reset Password',
+    };
+    const emailTemplate = await renderEmailTemplate(templatePath, replacements);
+    await this.sendMail(emailTemplate, `TourNest, Password Reset`);
+  }
+};
